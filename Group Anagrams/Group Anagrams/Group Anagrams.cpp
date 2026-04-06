110c7baf9c87a20e1a134bf295d4101b1288b179
Gonfod <vladqqqx@gmail.com>
2026-04-05 18:58:20 -0700

    Finished Group Anagrams task

---
+++ Group Anagrams/Group Anagrams/Group Anagrams.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+std::vector<std::vector<std::string>> groupAnagrams(std::vector<std::string>& strs) {
+    std::unordered_map<std::string, std::vector<std::string>> res;
+    for (const auto& s : strs) {
+        std::string sorted = s;
+        sort(sorted.begin(), sorted.end());
+        res[sorted].push_back(s);
+    }
+    std::vector<std::vector<std::string>> result;
+    for (auto& p : res )
+    {
+        result.push_back(p.second);
+    }
+    return result;
+
+}
